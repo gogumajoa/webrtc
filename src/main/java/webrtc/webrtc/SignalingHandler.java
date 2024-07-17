d057a704bfc3c8e47f7053afd57ff5febf42f8e0
최민 <rmdwjd76@naver.com>
2024-07-17 16:42:02 +0900

    test

---
+++ src/main/java/webrtc/webrtc/SignalingHandler.java
@@ -0,0 +1,39 @@
+package webrtc.webrtc;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SignalingHandler extends TextWebSocketHandler {
+
+    private static final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())) {
+                try {
+                    webSocketSession.sendMessage(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
